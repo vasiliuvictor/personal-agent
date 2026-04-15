2054ba496e8fb4ada8b2c98bf836d0d391e277e2
vasiliuvictor <v_andu@yahoo.com>
2026-04-15 12:44:15 +0200

    APP

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://agentscout-backend.onrender.com/api',
+  apiUrl: 'https://agentscout-backend-whye.onrender.com/api',
   supabaseUrl: 'https://kgbczdjhbllmbvuyuatk.supabase.co',
   supabaseAnonKey: 'sb_publishable_li9usd6ktX7UelNcbTjDcQ_WDuN8M_4',
 };
